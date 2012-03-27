9efba492971c872bc88fbefe8654261d7c123799
Cheryl Porter <cporter@ufl.edu>
2012-03-27 08:39:52 -0400

    v4.5.1.29 After merges and before new changes.

--- ModuleDefs.for
+++ ModuleDefs.for
@@ -61,12 +61,13 @@ C             CHP Added TRTNUM to CONTROL variable.
         INTEGER :: Major = 4
         INTEGER :: Minor = 5
         INTEGER :: Model = 1
-        INTEGER :: Build = 28
+        INTEGER :: Build = 29
       END TYPE VersionType
       TYPE (VersionType) Version
       CHARACTER(len=10) :: VBranch = '-Stub     '
 
 !     Version history:  
+!       4.5.1.29 chp 03/26/2012 New version number after merge and before additional changes.
 !       4.5.1.28 chp 03/13/2012 Synch with v4.6.0.13
 !       4.5.1.27 chp 02/20/2012 Ceres-rice temperature response changes, PHINT in cul file
 !       4.5.1.26 chp 02/01/2012 Revise BD, DUL, LL estimates due to OC changes
